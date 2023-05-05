d901e077145449f316020aff3e44e26268093e96
YehorSaz <inercoin@gmail.com>
2023-05-05 04:12:27 +0300

    add form

--- src/components/Lists/Lists.js
+++ src/components/Lists/Lists.js
@@ -1,19 +1,41 @@
 import React from 'react';
 
 import {TiPlusOutline} from "react-icons/ti";
+import {useForm} from "react-hook-form";
 
 const Lists = () => {
+
+    const {register, reset, handleSubmit} = useForm();
+
+        const formWrap = document.querySelector('.input-field');
+    const showForm = () => {
+        formWrap.style.cssText = `opacity: 1; z-index: 10`;
+    }
+    const hideForm = () => {
+        formWrap.style.cssText = `opacity: 0; z-index: -1`
+    }
 
     return (
 
         <div className={'lists'}>
             <h1>Lists</h1>
 
+
+            <div className={'input-field'}>
+
+                <form onSubmit={handleSubmit(hideForm)} className={'item-form'}>
+                    <input type="text" placeholder={'продукт'} {...register('item')}/>
+                    <button>зберегти</button>
+                </form>
+
+            </div>
+
+
             <div className={'list-buttons'}>
 
                 <div className={'mic-button'}>
 
-                    <button className={'new-item-button'}>
+                    <button className={'new-item-button'} onClick={() => showForm()}>
                         MIC
                     </button>
 
